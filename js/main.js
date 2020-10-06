d365d4579e0e4287fb36f609c4cfa263b2f8ce5d
Xenia Panayot <xpanayot@icloud.com>
2020-10-06 11:28:48 +0200

    Started main.js, added function creating a card of an offer

---
+++ js/main.js
@@ -0,0 +1,55 @@
+`use strict`;
+
+// constants
+
+const TITLE = [`Title1`, `Title2`, `Title3`];
+const PRICE = [100, 200, 300];
+const TYPE = [`palace`, `flat`, `house`, `bungalow`];
+const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const ROOMS = [1, 2, 3];
+const GUESTS = [1, 2, 3];
+const CHECKIN = [`12:00`, `13:00`, `14:00`];
+const CHECKOUT = [`12:00`, `13:00`, `14:00`];
+const DESCRIPTION = [`Description1`, `Description2`, `Description3`];
+const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+
+// functions to make randon number and random item
+const getRndInteger = function (min, max) {
+return Math.round(Math.random() * (max - min + 1) ) + min;
+};
+
+const getRndItem = function (items) {
+  const maxIndex = items.length - 1;
+  const rndIndex = getRndInteger(0, maxIndex);
+  return items[rndIndex];
+}
+getRndItem(DESCRIPTION);
+
+// the function to get the card
+
+const getCard = function () {
+  return {
+    author: {
+      avatar: `img/avatars/user` + 0 + getRndInteger(1,8) + `.png` // строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
+      },
+      offer: {
+        title: getRndItem(TITLE), // строка, заголовок предложения
+        address: `location.x location.y`, // строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
+        price: getRndItem(PRICE), // число, стоимость
+        type: getRndItem(TYPE), // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalow
+        rooms: getRndItem(ROOMS), // число, количество комнат
+        guests: getRndItem(GUESTS), // число, количество гостей, которое можно разместить
+        checkin: getRndItem(CHECKIN), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
+        checkout: getRndItem(CHECKOUT), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+        features: getRndItem(FEATURES), // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+        description: getRndItem(DESCRIPTION), // строка с описанием,
+        photos: getRndItem(PHOTOS), //  массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+    },
+    location: {
+        x: getRndInteger(),// случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
+        y: getRndInteger(130, 630),// случайное число, координата y метки на карте от 130 до 630.
+    }
+
+  }
+}
+
